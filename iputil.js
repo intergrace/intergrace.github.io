fde34733354056fd4c4071977d26a30fc6f724e3
Vadym <30234255+vadkor@users.noreply.github.com>
2022-12-20 22:28:39 +1100

    Replaced ipify cal with its IPv6 capable and JSONP

--- iputil.js
+++ iputil.js
@@ -1,7 +1,3 @@
-fetch("https://api.ipify.org/")
-  .then(function(res) {return res.text()})
-  .then(function(data) {
-    if (data) {
-      document.getElementById('downloader').value = data
-    }
-  })
+function callback(data) {
+  document.getElementById('downloader').value = data.ip
+}
